07b726b216c0261857a4829a75794f644cc8bfea
Annate20105 <niko.an.du@gmail.com>
2021-04-19 10:55:50 +0300

    update

--- Sources/KVKCalendar/MonthCell.swift
+++ Sources/KVKCalendar/MonthCell.swift
@@ -90,8 +90,13 @@ final class MonthCell: UICollectionViewCell {
             for (idx, event) in events.enumerated() {
                 let width = frame.width - 10
                 let count = idx + 1
-//                let label = UILabel(frame: CGRect(x: 5, y: 5 + dateLabel.bounds.height + height * CGFloat(idx), width: width, height: height))
-                let label = UILabel(frame: CGRect(x: 5 + width * CGFloat(idx), y: 5 + dateLabel.bounds.height , width: width, height: height))
+                let label = UILabel()
+                if  count > titlesCount {
+                    label.frame = CGRect(x: 5, y: 5 + dateLabel.bounds.height + height * CGFloat(idx), width: width, height: height)
+                } else {
+                    label.frame = CGRect(x: 5 + 20 * CGFloat(idx), y: 5 + dateLabel.bounds.height , width: width, height: height)
+                }
+
                 label.isUserInteractionEnabled = true
                 
                 if count > titlesCount {
